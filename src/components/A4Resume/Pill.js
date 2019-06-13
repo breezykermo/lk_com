7ba84adbf093f71e78bf11d8065c39e463d53557
Lachlan Kermode <lachiekermode@gmail.com>
2019-06-13 10:29:43 +0100

    add merantix, links to companies

--- src/components/A4Resume/Pill.js
+++ src/components/A4Resume/Pill.js
@@ -31,20 +31,38 @@ const styles = {
   },
 }
 
-const Pill = (props) => (
-  <div className="row" style={styles.pillOuter}>
-    <div style={styles.pillLeft}>
-      {props.left}
+const Pill = (props) => {
+  let splitTitle = false
+  let titleFst = null
+  let titleSnd = null
+  const spl = props.body.title.split(":")
+  if (spl.length === 2) {
+    splitTitle = true;
+    titleFst = spl[0]
+    titleSnd = spl[1]
+  }
+
+  return (
+    <div className="row" style={styles.pillOuter}>
+      <div style={styles.pillLeft}>
+        {props.left}
+      </div>
+      <div style={styles.pillBody}>
+          {!!props.body.link ? (
+            <h4 style={styles.subheader}>
+              <a href={props.body.link}>{splitTitle ? titleFst: props.body.title}</a>{splitTitle && `: ${titleSnd}`}
+            </h4>
+          ): (
+            <h4 style={styles.subheader}>{props.body.title}</h4>
+          )}
+        <p style={styles.pillParagraph}>{props.body.content}</p>
+      </div>
+      <div style={styles.pillRight}>
+        {props.right}
+      </div>
     </div>
-    <div style={styles.pillBody}>
-      <h4 style={styles.subheader}>{props.body.title}</h4>
-      <p style={styles.pillParagraph}>{props.body.content}</p>
-    </div>
-    <div style={styles.pillRight}>
-      {props.right}
-    </div>
-  </div>
-)
+  )
+}
 Pill.propTypes = {
   left: PropTypes.string,
   right: PropTypes.string,
